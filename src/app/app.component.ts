461bbd24d58468f4fb34e8b87057807c1a2bf04f
Martin Koller <kollermartin00@gmail.com>
2023-07-22 18:18:33 +0200

    Configure eslint + prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'code-sage';
+  title =
+    'code-sage das das das das das das das das das das das das das das as das das das das das das a das das das das das d';
 }
