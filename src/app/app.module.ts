e21a775bfc15ec94938969aee14f412ec7826da0
Martin Koller <kollermartin00@gmail.com>
2023-07-24 19:52:27 +0200

    create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { CoreModule } from './core/core.module';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentLayoutComponent],
+  declarations: [AppComponent, ContentLayoutComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
