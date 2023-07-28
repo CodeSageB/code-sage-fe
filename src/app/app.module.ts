98769b7d6b434928d2453e3db7f25ce3be8a6229
Martin Koller <kollermartin00@gmail.com>
2023-07-28 20:30:50 +0200

    Install ngx-translate, add localisation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,32 @@ import { MaterialModule } from './shared/material.module';
 import { CoreModule } from './core/core.module';
 import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent, ContentLayoutComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    CoreModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
