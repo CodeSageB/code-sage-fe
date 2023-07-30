e5b29aaa0004c409355f9f6333b51d31cd1c5bbc
Martin Koller <kollermartin00@gmail.com>
2023-07-30 15:19:25 +0200

    Add unit tests

--- src/app/shared/pipes/available-langs.pipe.spec.ts
+++ src/app/shared/pipes/available-langs.pipe.spec.ts
@@ -1,8 +1,20 @@
 import { AvailableLangsPipe } from './available-langs.pipe';
+import { LanguagesEnum } from '../../data/schema/languages.enum';
 
 describe('AvailableLangsPipe', () => {
   it('create an instance', () => {
     const pipe = new AvailableLangsPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('expect to get list with all languages but active language', () => {
+    const allLanguages = Object.values(LanguagesEnum);
+    const activeLanguage = LanguagesEnum.CS;
+    const pipe = new AvailableLangsPipe();
+
+    const availableLanguages = pipe.transform(allLanguages, activeLanguage);
+
+    expect(availableLanguages).not.toContain(activeLanguage);
+    expect(availableLanguages.length).not.toBe(allLanguages.length);
   });
 });
--- src/app/shared/services/localisation.service.spec.ts
+++ src/app/shared/services/localisation.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LocalisationService } from './localisation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('LocalisationService', () => {
   let service: LocalisationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+    });
     service = TestBed.inject(LocalisationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('SetLang function works', () => {
+    service.setLang('cs');
+
+    expect(service.currentLangSignal()).toBe('cs');
   });
 });
