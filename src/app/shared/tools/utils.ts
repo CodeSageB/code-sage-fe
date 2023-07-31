df10dc875d1db745e8962b1bb622e86f918b00c3
Martin Koller <kollermartin00@gmail.com>
2023-07-31 21:34:36 +0200

    Change order of menu items

--- src/app/shared/tools/utils.ts
+++ src/app/shared/tools/utils.ts
@@ -3,8 +3,8 @@ import { MenuItem } from '../../data/schema/menu-item';
 export class Utils {
   public static readonly MENU_ITEMS: MenuItem[] = [
     { name: 'home' },
-    { name: 'about' },
     { name: 'blog' },
+    { name: 'about' },
     { name: 'newsletter', color: 'primary' },
   ];
 }
