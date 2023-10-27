52d2297cc808eff5cff73e4f19f82938bf0050b6
Martin Koller <koller.martin@betsys.com>
2023-10-27 21:53:44 +0200

    feat: fix tests

--- src/app/shared/services/blogs.service.spec.ts
+++ src/app/shared/services/blogs.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BlogsService } from './blogs.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('BlogsService', () => {
   let service: BlogsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(BlogsService);
   });
 
